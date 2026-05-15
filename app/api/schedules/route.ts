1538b78e1b0f80b06a7efbb71e6dacdb40b5fe5e
eimanncalderon0711 <calderon.eimann0711@gmail.com>
2026-05-15 23:32:12 +0800

    fix the api

--- app/api/schedules/route.ts
+++ app/api/schedules/route.ts
@@ -2,6 +2,6 @@ import { NextResponse } from "next/server"
 import { scheduleService } from "@/services/schedule.service"
 
 export async function GET() {
-  const data = await scheduleService.getSchedule()
+  const data = await scheduleService.getAll({});
   return NextResponse.json(data)
 }
